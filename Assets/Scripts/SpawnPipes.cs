5e59df2c56c17ce678c37def00941645ea4d72d7
rohith <rohithathrangadan1999@gmail.com>
2023-02-19 12:00:31 +0530

    updated SpawnPipes Script for random pipe position

--- Assets/Scripts/SpawnPipes.cs
+++ Assets/Scripts/SpawnPipes.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class SpawnPipes : MonoBehaviour
@@ -7,20 +5,25 @@ public class SpawnPipes : MonoBehaviour
     float timeBtwSpawn;
     public float resetTimeBtwSpawn;
     public GameObject pipes;
+    float Ypos;
+    Vector3 spawnPos;
     // Start is called before the first frame update
     void Start()
     {
-        timeBtwSpawn = resetTimeBtwSpawn;
-        Instantiate(pipes, transform.position, transform.rotation);
-
+        //timeBtwSpawn = resetTimeBtwSpawn;
+        //Instantiate(pipes, transform.position, transform.rotation);
     }
 
     // Update is called once per frame
     void Update()
     {
+        Ypos = Random.Range(-1.63f, 1.59f); // randomize the position of pipes
+        spawnPos = new Vector3(transform.position.x, Ypos, transform.position.z);
+
         if (timeBtwSpawn <= 0)
         {
-            Instantiate(pipes, transform.position, transform.rotation);
+            Instantiate(pipes, spawnPos, transform.rotation);
+            //Instantiate takes Vector3 values
             timeBtwSpawn = resetTimeBtwSpawn;
         }
         else
