7757d0081fa28d3099ac06986715e9fff46fd198
rohith <rohithathrangadan1999@gmail.com>
2023-02-19 11:13:26 +0530

    Updated SpawnPipes Script by setting timer for spawn.

--- Assets/Scripts/SpawnPipes.cs
+++ Assets/Scripts/SpawnPipes.cs
@@ -4,16 +4,26 @@ using UnityEngine;
 
 public class SpawnPipes : MonoBehaviour
 {
+    float timeBtwSpawn;
+    public float resetTimeBtwSpawn;
     public GameObject pipes;
     // Start is called before the first frame update
     void Start()
     {
+        timeBtwSpawn = resetTimeBtwSpawn;
         Instantiate(pipes, transform.position, transform.rotation);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (timeBtwSpawn <= 0)
+        {
+            Instantiate(pipes, transform.position, transform.rotation);
+            timeBtwSpawn = resetTimeBtwSpawn;
+        }
+        else
+            timeBtwSpawn -= Time.deltaTime;
     }
 }
